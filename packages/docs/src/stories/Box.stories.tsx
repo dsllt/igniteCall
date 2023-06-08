a8c751dbcd5dd8339e3327bec4ec9b790430cb0a
dsllt <dayanesallet@hotmail.com>
2023-06-08 18:27:24 -0300

    first commit

---
+++ packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Box, BoxProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Surface/Box',
+  component: Box,
+  args: {
+    children: (
+      <>
+        <span>Testando o elemento box</span>
+      </>
+    ),
+  },
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {}
