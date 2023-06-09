3019f11d6cd9d6d977110675566a8a898e364bfc
dsllt <dayanesallet@hotmail.com>
2023-06-09 05:50:41 -0300

    Criar componente text

--- packages/docs/src/stories/Box.stories.tsx
+++ packages/docs/src/stories/Box.stories.tsx
@@ -1,15 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { Box, BoxProps } from '@ignite-ui/react'
+import { Box, BoxProps, Text } from '@ignite-ui/react'
 
 export default {
   title: 'Surface/Box',
   component: Box,
   args: {
-    children: (
-      <>
-        <span>Testando o elemento box</span>
-      </>
-    ),
+    children: <Text>Testando o elemento box</Text>,
   },
 } as Meta<BoxProps>
 
